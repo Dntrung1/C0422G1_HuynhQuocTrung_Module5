b131b2676eb207cf9bff1ae348433874bd4efc16
dntrung1 <dntrung1@gmail.com>
2022-09-25 20:14:51 +0700

    upcase

--- Furama_Casestudy/Furama/src/app/app.module.ts
+++ Furama_Casestudy/Furama/src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { FacilityListComponent } from './facility-list/facility-list.component';
+import { FacilityCreateComponent } from './facility-create/facility-create.component';
+import { FacilityUpdateComponent } from './facility-update/facility-update.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerCreateComponent } from './customer-create/customer-create.component';
+import { CustomerUpdateComponent } from './customer-update/customer-update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    FacilityListComponent,
+    FacilityCreateComponent,
+    FacilityUpdateComponent,
+    CustomerListComponent,
+    CustomerCreateComponent,
+    CustomerUpdateComponent
   ],
   imports: [
     BrowserModule,
