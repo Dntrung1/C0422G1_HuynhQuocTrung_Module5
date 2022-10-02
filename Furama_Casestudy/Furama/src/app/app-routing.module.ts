e1982cde3de25fb5b5fc1c3a894591aab8619237
dntrung1 <dntrung1@gmail.com>
2022-10-03 04:26:39 +0700

    update

--- Furama_Casestudy/Furama/src/app/app-routing.module.ts
+++ Furama_Casestudy/Furama/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {FacilityListComponent} from "./facility-list/facility-list.component";
+import {CustomerListComponent} from "./customer-list/customer-list.component";
+import {FacilityCreateComponent} from "./facility-create/facility-create.component";
+import {FacilityUpdateComponent} from "./facility-update/facility-update.component";
+import {CustomerCreateComponent} from "./customer-create/customer-create.component";
+import {CustomerUpdateComponent} from "./customer-update/customer-update.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"facility/list", component:FacilityListComponent},
+  {path:"customer/list", component:CustomerListComponent},
+  {path:"facility/create", component:FacilityCreateComponent},
+  {path:"facility/update", component:FacilityUpdateComponent},
+  {path:"customer/create", component:CustomerCreateComponent},
+  {path:"customer/update", component:CustomerUpdateComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
